6cec453f3358dfcf9a2dfd49f3cff3a14f90031c
Matteo <matteoc8@live.com>
2022-04-15 16:26:37 +0200

    Removed a for of print; added a unique print

--- src/main/java/SimpleClassForLogTest.java
+++ src/main/java/SimpleClassForLogTest.java
@@ -2,9 +2,6 @@ public class SimpleClassForLogTest {
     public static void main(String[] args){
         System.out.println("This is the first commit");
         System.out.println("Added new print");
-        int i;
-        for(i=0;i<10;i++){
-            System.out.println("Test");
-        }
+        System.out.println("Test");
     }
 }
