aacaa09d2888f6a63fd8998f59f3a57b130a7926
Matteo <matteoc8@live.com>
2022-04-15 16:24:13 +0200

    added new print

--- src/main/java/SimpleClassForLogTest.java
+++ src/main/java/SimpleClassForLogTest.java
@@ -1,5 +1,6 @@
 public class SimpleClassForLogTest {
     public static void main(String[] args){
         System.out.println("This is the first commit");
+        System.out.println("Added new print");
     }
 }
