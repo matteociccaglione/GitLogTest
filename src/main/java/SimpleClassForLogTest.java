53c725a414c53858d1c03f42b42e050e6b2e6918
Matteo <matteoc8@live.com>
2022-04-15 16:26:05 +0200

    added a for of print

--- src/main/java/SimpleClassForLogTest.java
+++ src/main/java/SimpleClassForLogTest.java
@@ -2,5 +2,9 @@ public class SimpleClassForLogTest {
     public static void main(String[] args){
         System.out.println("This is the first commit");
         System.out.println("Added new print");
+        int i;
+        for(i=0;i<10;i++){
+            System.out.println("Test");
+        }
     }
 }
