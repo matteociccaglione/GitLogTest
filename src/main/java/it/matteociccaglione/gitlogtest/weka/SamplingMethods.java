dc6ad8bca11fbc19486d250827f893d438bb8368
Matteo <matteoc8@live.com>
2022-05-26 12:25:58 +0200

    Added code for sampling and filtering

---
+++ src/main/java/it/matteociccaglione/gitlogtest/weka/SamplingMethods.java
@@ -0,0 +1,7 @@
+package it.matteociccaglione.gitlogtest.weka;
+
+public enum SamplingMethods {
+    RESAMPLE,
+    SMOTE,
+    SPREADSUBSAMPLE
+}
--- src/main/java/it/matteociccaglione/gitlogtest/weka/WekaManager.java
+++ src/main/java/it/matteociccaglione/gitlogtest/weka/WekaManager.java
@@ -2,17 +2,27 @@ package it.matteociccaglione.gitlogtest.weka;
 
 import it.matteociccaglione.gitlogtest.file.FileBuilder;
 import it.matteociccaglione.gitlogtest.jira.Version;
+import weka.classifiers.Classifier;
+import weka.classifiers.meta.FilteredClassifier;
+import weka.filters.Filter;
+import weka.filters.supervised.attribute.AttributeSelection;
+import weka.attributeSelection.CfsSubsetEval;
+import weka.attributeSelection.GreedyStepwise;
 import weka.classifiers.Evaluation;
 import weka.classifiers.bayes.NaiveBayes;
 import weka.classifiers.lazy.IBk;
 import weka.classifiers.trees.RandomForest;
+import weka.core.Instance;
 import weka.core.Instances;
 import weka.core.converters.ConverterUtils;
+import weka.filters.supervised.instance.Resample;
+import weka.filters.supervised.instance.SpreadSubsample;
 import weka.gui.beans.DataSource;
-
 import java.io.File;
 import java.io.IOException;
 import java.util.List;
+
+import static it.matteociccaglione.gitlogtest.weka.SamplingMethods.SMOTE;
 
 public class WekaManager {
     public static String toArff(List<Version> versions, String filename) throws IOException {
@@ -31,6 +41,10 @@ public class WekaManager {
         Instances iTraining = dataTraining.getDataSet();
         ConverterUtils.DataSource dataTesting = new ConverterUtils.DataSource(testing);
         Instances iTesting = dataTesting.getDataSet();
+        return WekaManager.classifier(iTraining,iTesting,classifier,classifiers);
+
+    }
+    public static WekaResults classifier(Instances iTraining, Instances iTesting, weka.classifiers.Classifier classifier, Classifiers classifiers) throws Exception {
         weka.classifiers.Classifier cl = classifier;
         iTraining.setClassIndex(iTraining.numAttributes()-1);
         cl.buildClassifier(iTraining);
@@ -38,6 +52,41 @@ public class WekaManager {
         Evaluation eval = new Evaluation(iTesting);
         eval.evaluateModel(cl,iTesting);
         return new WekaResults(eval.areaUnderROC(1), eval.recall(1),eval.precision(1),eval.kappa(),classifiers);
+    }
 
+    public static WekaResults sampling(String training, String testing, weka.classifiers.Classifier classifier, Classifiers classifiers, SamplingMethods samplingMethods) throws Exception {
+        ConverterUtils.DataSource dataTraining = new ConverterUtils.DataSource(training);
+        Instances iTraining = dataTraining.getDataSet();
+        ConverterUtils.DataSource dataTesting = new ConverterUtils.DataSource(testing);
+        Instances iTesting = dataTesting.getDataSet();
+        Resample resample = new Resample();
+        SpreadSubsample spreadSubsample = new SpreadSubsample();
+        resample.setInputFormat(iTraining);
+        FilteredClassifier fc = new FilteredClassifier();
+        fc.setClassifier(classifier);
+        if(samplingMethods == SamplingMethods.RESAMPLE){
+            fc.setFilter(resample);
+        }
+        if(samplingMethods == SamplingMethods.SPREADSUBSAMPLE){
+            fc.setFilter(spreadSubsample);
+        }
+        fc.buildClassifier(iTraining);
+        Evaluation eval = new Evaluation(iTesting);
+        eval.evaluateModel(fc,iTesting);
+        return new WekaResults(eval.areaUnderROC(1), eval.recall(1),eval.precision(1),eval.kappa(),classifiers);
+    }
+
+    public static Instances featureSelection(String dataSource, boolean backward) throws Exception {
+        ConverterUtils.DataSource dataTraining = new ConverterUtils.DataSource(dataSource);
+        Instances iTraining = dataTraining.getDataSet();
+        CfsSubsetEval eval = new CfsSubsetEval();
+        AttributeSelection filter = new AttributeSelection();
+        GreedyStepwise search = new GreedyStepwise();
+        search.setSearchBackwards(backward);
+        filter.setEvaluator(eval);
+        filter.setSearch(search);
+        filter.setInputFormat(iTraining);
+        Instances filteredTraining = Filter.useFilter(iTraining,filter);
+        return filteredTraining;
     }
 }
